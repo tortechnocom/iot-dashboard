0ac7e627fdd99b1c4d27e02a576e455eab40a80b
Supachai Chaimangua <tortechnocom@gmail.com>
2015-11-02 23:11:33 +0700

    Update thing process

--- app/controllers/thing.js
+++ app/controllers/thing.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -12,42 +11,51 @@ var utils = require('../../lib/utils');
  * Create user
  */
 
-exports.create = function (req, res) {
-    req.body.user_id = req.user._id;
-    var thing = new Thing(req.body);
-    var errJson = {};
-    thing.save(function (err) {
-        if (err) {
-            console.log(err.message);
-            errJson = err.errors;
-            return res.json(err);
-        } else {
-            return res.json({});
-        }
-    });
+exports.create = function(req, res) {
+	req.body.user_id = req.user._id;
+	var thing = new Thing(req.body);
+	var errJson = {};
+	thing.save(function(err) {
+		if (err) {
+			console.log(err.message);
+			errJson = err.errors;
+			return res.json(err);
+		} else {
+			return res.json({});
+		}
+	});
 };
 
-exports.setting = function (req, res) {
-    setLang(req, res);
-    var things = null;
-    Thing.findOne({_id: req.query.thing_id}, function(err, thing) {
-        User.findOne({_id: req.query.user_id}, function(err, user) {
-            res.render('thing/thing-setting', {
-                thing: thing,
-                user: user
-            });
-        });
-    });
-  };
+exports.setting = function(req, res) {
+	setLang(req, res);
+	var things = null;
+	Thing.findOne({
+		_id : req.query.thing_id
+	}, function(err, thing) {
+		User.findOne({
+			_id : req.query.user_id
+		}, function(err, user) {
+			res.render('thing/thing-setting', {
+				thing : thing,
+				user : user
+			});
+		});
+	});
+};
 
-exports.update = function (req, res) {
-      Thing.findOneAndUpdate({_id: req.body.thing_id}, {$set: req.body}, function (err, doc) {
-          if (err) {
-              console.log(err.message);
-              errJson = err.errors;
-              return res.json(err);
-          } else {
-              return res.json({});
-          }
-      });
-  };
+exports.update = function(req, res) {
+	req.body.enabled = (req.body.enabled != null ? req.body.enabled : "N");
+	Thing.findOneAndUpdate({
+		_id : req.body.thing_id
+	}, {
+		$set : req.body
+	}, function(err, doc) {
+		if (err) {
+			console.log(err.message);
+			errJson = err.errors;
+			return res.json(err);
+		} else {
+			return res.json({});
+		}
+	});
+};
